e6c153854b8b0a0e74fe38b5ba0d4c6ec37197fa
Simon Bouchard <simon.bouchard31@gmail.com>
2025-01-12 21:10:49 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const multer = require('multer');
+//const { fileTypeFromfile  } = require('file-type');
 
 var app = express();
 
@@ -11,7 +13,20 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const upload = multer({dest: 'uploads/'});
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+	if (!req.file) {
+		    return res.status(400).send('No file uploaded');
+	}
+	try {
+		const {originalname, mimetype, size} = req.file;
+		return res.json({name: originalname, type: mimetype, size: size});
+	} catch (err) {
+		console.error('Error processing file:', err);
+	    res.status(500).send('Failed to process file');
+	}
+});
 
 
 const port = process.env.PORT || 3000;
